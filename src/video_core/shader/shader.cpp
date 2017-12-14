1799f24dcb12107d4bb56000de50a2d7efacf557
Dwayne Slater <ds84182@gmail.com>
2017-12-13 21:51:16 -0500

    Addressed more review comments

--- src/video_core/shader/shader.cpp
+++ src/video_core/shader/shader.cpp
@@ -24,9 +24,9 @@ namespace Shader {
 void OutputVertex::ValidateSemantics(const RasterizerRegs& regs) {
     unsigned int num_attributes = regs.vs_output_total;
     ASSERT(num_attributes <= 7);
-    for (size_t i = 0; i < num_attributes; ++i) {
-        u32 output_register_map = regs.vs_output_attributes[i].raw;
-        for (size_t j = 0; j < 4; j++) {
+    for (size_t attrib = 0; attrib < num_attributes; attrib++) {
+        u32 output_register_map = regs.vs_output_attributes[attrib].raw;
+        for (size_t comp = 0; comp < 4; comp++) {
             u32 semantic = output_register_map & 0x1F;
             ASSERT_MSG(semantic < 24 || semantic == RasterizerRegs::VSOutputAttributes::INVALID,
                        "Invalid/unknown semantic id: %u", semantic);
@@ -41,18 +41,20 @@ OutputVertex OutputVertex::FromAttributeBuffer(const RasterizerRegs& regs,
     union {
         OutputVertex ret{};
         std::array<float24, 24> vertex_slots;
-        // Allow us to overflow vertex_slots to avoid branches
+        // Allow us to overflow vertex_slots to avoid branches, since
+        // RasterizerRegs::VSOutputAttributes::INVALID would write to slot 31, which
+        // would be out of bounds otherwise.
         std::array<float24, 32> vertex_slots_overflow;
     };
     static_assert(sizeof(vertex_slots) == sizeof(ret), "Struct and array have different sizes.");
 
     unsigned int num_attributes = regs.vs_output_total & 7;
-    for (size_t i = 0; i < num_attributes; ++i) {
-        u32 output_register_map = regs.vs_output_attributes[i].raw;
-        vertex_slots_overflow[output_register_map & 0x1F] = input.attr[i][0];
-        vertex_slots_overflow[(output_register_map >> 8) & 0x1F] = input.attr[i][1];
-        vertex_slots_overflow[(output_register_map >> 16) & 0x1F] = input.attr[i][2];
-        vertex_slots_overflow[(output_register_map >> 24) & 0x1F] = input.attr[i][3];
+    for (size_t attrib = 0; attrib < num_attributes; attrib++) {
+        u32 output_register_map = regs.vs_output_attributes[attrib].raw;
+        vertex_slots_overflow[output_register_map & 0x1F] = input.attr[attrib][0];
+        vertex_slots_overflow[(output_register_map >> 8) & 0x1F] = input.attr[attrib][1];
+        vertex_slots_overflow[(output_register_map >> 16) & 0x1F] = input.attr[attrib][2];
+        vertex_slots_overflow[(output_register_map >> 24) & 0x1F] = input.attr[attrib][3];
     }
 
     // The hardware takes the absolute and saturates vertex colors like this, *before* doing
